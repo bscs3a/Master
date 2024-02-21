f7126ed044bd7521302586d7c069700c9348694e
bakasibruceto <abla09027@gmail.com>
2024-02-22 05:32:52 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+  <link href="./src/tailwind.css" rel="stylesheet">
+</head>
+<body>
+  <h1 class="text-xl  items-center font-bold  bg-red-900  hover:text-green-500">
+   banana
+  </h1>
+
+</body>
+</html>
