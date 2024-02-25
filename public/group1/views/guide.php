3a7659613ca1d3bf2a54466afc9a8333a0974d67
bakasibruceto <abla09027@gmail.com>
2024-02-25 17:29:09 +0800

    Point of Sales Guide

---
+++ public/group1/views/guide.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="./../../src/tailwind.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-slate-200">
+<h1 class="text-center text-4xl itali">Point of Sales</h1>
+
+</body>
+
+</html>
