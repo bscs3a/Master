1c56d6b3a0a2c94c96fdbd254b8c4e419b953e2a
bakasibruceto <abla09027@gmail.com>
2024-02-25 17:29:32 +0800

    GET router

---
+++ router.php
@@ -0,0 +1,22 @@
+<?php
+
+class Router
+{
+    public static function handle($method = 'GET', $path = '/', $filename =''){
+        // print_r($_SERVER);
+        $currentMethod = $_SERVER['REQUEST_METHOD'];
+        $currentUri = $_SERVER['REQUEST_URI'];
+        // echo $path;
+        if($currentMethod != $method ){
+            require $filename;
+        }
+        $root = '/Master';
+        $pattern = '#^'.$root.$path.'$#sD';
+        if(preg_match($pattern, $currentUri)){
+            require_once $filename;
+            exit();
+        }
+
+        return false;
+    }
+}
