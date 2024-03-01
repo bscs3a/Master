f33e3ef69af5778657fff14c0ed52bf33f1dd1a9
Isarakyun <119465506+Isarakyun@users.noreply.github.com>
2024-03-01 16:04:49 +0800

    sample (admin) updated
    example for groupmates

--- public/admin/views/adm.test-link.php
+++ public/admin/views/adm.test-link.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./../src/tailwind.css" rel="stylesheet">
+    <link href="../../../src/tailwind.css" rel="stylesheet">
     <title>Test Link</title>
 </head>
 
 <body>
     <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <h1 class="mb-6 text-3xl font-bold text-center text-blue-500">This is a Test-link Routing for Admin</h1>
-        <button onclick="location.href='/Master/adm/sample'"
+        <button onclick="location.href='adm.sample.php'"
             class="px-6 py-3 text-white bg-blue-500 rounded hover:bg-blue-700">Go to Admin page</button>
     </div>
 </body>
