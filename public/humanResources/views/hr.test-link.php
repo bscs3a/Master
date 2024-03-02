d45124435afb7d52444f2436b292ca522e1e1583
Isarakyun <119465506+Isarakyun@users.noreply.github.com>
2024-03-02 12:59:06 +0800

    reverted changes

--- public/humanResources/views/hr.test-link.php
+++ public/humanResources/views/hr.test-link.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../../../src/tailwind.css" rel="stylesheet">
+    <link href="./../src/tailwind.css" rel="stylesheet">
     <title>Test Link</title>
 </head>
 
