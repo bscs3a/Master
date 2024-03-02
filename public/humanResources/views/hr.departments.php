d71efda1f56ec915d885e07bb2a913c6da9b9c65
Isarakyun <119465506+Isarakyun@users.noreply.github.com>
2024-03-02 12:27:05 +0800

    initial hr features added

---
+++ public/humanResources/views/hr.departments.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../../src/tailwind.css" rel="stylesheet">
+    <title>Departments</title>
+</head>
+<body>
+    
+    <!-- view departments (employees in inventory, sales, accounting, delivery) -->
+    <div 
+    class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1
+            class="mb-6 text-3xl font-bold text-center text-blue-500">
+            Departments here
+        </h1>
+    </div>
+
+</body>
+</html>
