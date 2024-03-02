c00e7e6b9663b52b60f2d7af39ff7fae2861bcd4
Jake Ashley Panlilio <2021300973@dhvsu.edu.ph>
2024-03-02 17:56:57 +0800

    UPDATED ACCORDING TO MASTER FILE

--- public/delivery/views/dlv.sample.php
+++ public/delivery/views/dlv.sample.php
@@ -10,10 +10,10 @@
 </head>
 
 <body>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-blue-100">
+  <div class="flex flex-col items-center justify-center min-h-screen bg-green-700">
     <h1 class="mb-6 text-3xl font-bold text-center text-blue-500">Sample Delivery Page</h1>
     <button onclick="location.href='/Master/dlv/link'"
-      class="px-6 py-3 text-white bg-blue-500 rounded hover:bg-blue-700">Go to Link Test</button>
+      class="px-6 py-3 text-white bg-blue-500 rounded hover:bg-blue-700">Go to Link Test a</button>
   </div>
 
 </body>
--- public/inventory/routes.php
+++ public/inventory/routes.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 
 $path = './public/inventory/views';
 
 Router::handle('GET', '/inv/sample', "$path/inv.sample.php");
 Router::handle('GET', '/inv/link', "$path/inv.test-link.php");
+Router::handle('GET', '/inv/view', "$path/inv.view.php");
 
