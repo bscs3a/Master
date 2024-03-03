cfa2484c516f12aa155d907534429ebbf14f9358
vvincerr <2021301469@dhvsu.edu.ph>
2024-03-03 09:10:03 +0800

    1

--- public/delivery/views/dlv.sample.php
+++ public/delivery/views/dlv.sample.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Delivery Management</title>
     <!-- Include Tailwind CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="./../src/tailwind.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100">
 
