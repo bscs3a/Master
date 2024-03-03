4f9d6591489636f754bc63e266ecff0609b6e07a
aries joseph tagle <ajtagle12@gmail.com>
2024-03-03 18:26:32 +0800

    inayos sql nagdagdag ng functionTesting

---
+++ public/finance/functions/functionTesting.php
@@ -0,0 +1,8 @@
+<?php
+// for testing purposes
+// run function in the command line using
+// php -r "require 'public\finance\functions\functionTesting.php';"  
+
+    require_once "assets/foo.php";
+    echo BoughtInventory(); //change this to the function you want to test
+?>
