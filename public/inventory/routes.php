466d39e3eb519d58d9d8389b960f3401f04c6da4
Jake Ashley Panlilio <2021300973@dhvsu.edu.ph>
2024-03-05 16:49:53 +0800

    UPDATE

--- public/inventory/routes.php
+++ public/inventory/routes.php
@@ -2,7 +2,9 @@
 
 $path = './public/inventory/views';
 
+
 Router::handle('GET', '/inv/sample', "$path/inv.sample.php");
-Router::handle('GET', '/inv/link', "$path/inv.test-link.php");
 Router::handle('GET', '/inv/view', "$path/inv.view.php");
+Router::handle('GET', '/inv/prod_ord', "$path/inv.prod_ord.php");
+
 
--- public/inventory/views/inv.sample.php
+++ public/inventory/views/inv.sample.php
@@ -12,6 +12,7 @@
   HELLO WORLD
   MENU
   <div>
+    <a href="/Master/inv/prod_ord" class="px-6 py-3 text-white bg-blue-500 rounded hover:bg-blue-700">product order</a>
     <a href="/Master/inv/view" class="px-6 py-3 text-white bg-blue-500 rounded hover:bg-blue-700">VIEW</a>
   </div>
 </body>
