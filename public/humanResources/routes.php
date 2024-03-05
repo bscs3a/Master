381a5aedb3b55c862b879464284b454f93289fb5
Isarakyun <119465506+Isarakyun@users.noreply.github.com>
2024-03-05 20:46:03 +0800

    small changes

--- public/humanResources/routes.php
+++ public/humanResources/routes.php
@@ -4,5 +4,6 @@ $path = './public/humanResources/views';
 
 Router::handle('GET', '/hr/sample', "$path/hr.sample.php");
 Router::handle('GET', '/hr/link', "$path/hr.test-link.php");
+
 Router::handle('GET', '/hr/dashboard', "$path/hr.dashboard.php");
 
