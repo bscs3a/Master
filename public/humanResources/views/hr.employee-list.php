8d6d23a45075f520d62edc066f5390dd086f6d41
Isarakyun <119465506+Isarakyun@users.noreply.github.com>
2024-03-05 23:25:51 +0800

    sidenav + more hr features

---
+++ public/humanResources/views/hr.employee-list.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet"/>
+  <link href="./../src/tailwind.css" rel="stylesheet">
+  <title>List of Employees</title>
+</head>
+<body class="text-gray-800 font-sans">
+
+<!-- sidenav -->
+<?php 
+  require 'inc/sidenav.php';
+?>
+<!-- end of sidenav -->
+</body>
+</html> 
