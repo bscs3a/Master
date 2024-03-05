ad03bdeababf5acd59c85642b40df761403c07e4
bakasibruceto <abla09027@gmail.com>
2024-03-05 18:01:16 +0800

    added comments

--- public/finance/routes.php
+++ public/finance/routes.php
@@ -6,7 +6,7 @@ Router::handle('GET', '/fin/sample', "$path/fin.sample.php");
 Router::handle('GET', '/fin/link', "$path/fin.test-link.php");
 Router::handle('GET', '/fin/link2', "$path/fin.test-link2.php");
 
-
+// Route for Ledger General Page
 Router::handle('GET', '/fin/ledger', "$path/fin.ledger.php");
 
 
