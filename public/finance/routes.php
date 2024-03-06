9137c6eb1a056eb45212aec946813d81514a99dd
bakasibruceto <abla09027@gmail.com>
2024-03-07 00:26:08 +0800

    added all ledgers Routes

--- public/finance/routes.php
+++ public/finance/routes.php
@@ -6,8 +6,10 @@ Router::handle('GET', '/fin/sample', "$path/fin.sample.php");
 Router::handle('GET', '/fin/link', "$path/fin.test-link.php");
 Router::handle('GET', '/fin/link2', "$path/fin.test-link2.php");
 
-
-Router::handle('GET', '/fin/ledger', "$path/fin.ledger.php");
+// Routes for Ledger Page
+Router::handle('GET', '/fin/ledger', "$path/fin.ledger.gen.php");
+Router::handle('GET', '/fin/ledger/accounts/investors', "$path/fin.ledger.investors.php");
+Router::handle('GET', '/fin/ledger/accounts/payable', "$path/fin.ledger.payable.php");
 
 
 // Route for Dashboard Page
