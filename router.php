61be8bc3ce0e89b7a6f0bc0149c685909e9b2078
bakasibruceto <abla09027@gmail.com>
2024-03-07 09:22:36 +0800

    Create router.php

---
+++ router.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__)->load();
+
+class Router
+{
+    public static function handle($method = 'GET', $path = '/', $filename =''){
+        // print_r($_SERVER);
+        $currentMethod = $_SERVER['REQUEST_METHOD'];
+        $currentUri = $_SERVER['REQUEST_URI'];
+        // echo $path; 
+        if($currentMethod != $method ){
+            require $filename;
+        }
+        // $root = getenv('PROXY_ADDRESS');
+        $root = "/Master";
+        $pattern = '#^'.$root.$path.'$#sD';
+        if(preg_match($pattern, $currentUri)){
+            require_once $filename;
+            exit();
+        }
+
+        return false;
+    }
+}
+
