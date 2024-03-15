4d4780f67788c4c0558452527df27024bc231a60
lamed <94333231+lamed47@users.noreply.github.com>
2024-03-15 11:56:19 +0800

    components, minor adjustments, and suppliers.
    - adding sidebar components.
    - minor adjustments such as adding route for admin page, changing some href, and removing the previous code.
    - adding a ui for the suppliers info.
    
    Co-Authored-By: Cylie <160600645+Print-Saylee@users.noreply.github.com>
    Co-Authored-By: key-yan <131762936+kaiii07@users.noreply.github.com>
    Co-Authored-By: _krieshamaeee <131770431+krieshaaa@users.noreply.github.com>
    Co-Authored-By: Bhaddie-Habibi <131437156+Bhaddie-Habibi@users.noreply.github.com>

--- public/admin/routes.php
+++ public/admin/routes.php
@@ -2,8 +2,8 @@
 
 $path = './public/admin/views';
 
-Router::handle('GET', '/adm/dashboard', "$path/dashboard.php");
-Router::handle('GET', '/adm/edit-employee', "$path/edit-employee.php");
+Router::handle('GET', '/adm/dashboard', "$path/adm.dashboard.php");
+Router::handle('GET', '/adm/suppliers', "$path/adm.suppliers.php");
 Router::handle('GET', '/adm/product', "$path/product.php");
 Router::handle('GET', '/adm/login', "$path/adm.login.php");
 
