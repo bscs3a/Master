a91c4a3a4c0429faf77e74d4d84b46f04c076617
bakasibruceto <abla09027@gmail.com>
2024-03-17 16:17:11 +0800

    post test

--- public/finance/routes.php
+++ public/finance/routes.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
+
+
+
 $path = './public/finance/views';
 
 $basePath = "$path/fin.";
@@ -17,29 +20,39 @@ $fin = [
     '/fin/ledger' => $basePath . "ledger.gen.php",
     '/fin/ledger/accounts/investors' => $basePath . "ledger.investors.php",
     '/fin/ledger/accounts/payable' => $basePath . "ledger.payable.php",
-    
+
     //request
     '/fin/request' => $basePath . "requestInventory.php",
     '/fin/salary' => $basePath . "requestSalary.php",
 
     '/fin/test' => $basePath . "test.php",
-    
+
 
     '/' => "C:/xampp/htdocs/Master/index.php",
-    
+
 ];
 
 Router::post('/fin/test', function () {
+    $db = Database::getInstance();
+    $conn = $db->connect();
+
     $name = $_POST['name'];
- 
+
     // Validate the input
-    if (empty($name)) {
+    if (empty ($name)) {
         echo "Name and email are required.";
         return;
     }
     $_SESSION['name'] = $name;
-    
-    echo "Form submitted successfully. Hello, " . $name . "!";
+
+    $stmt = $conn->prepare("INSERT INTO name (name) VALUES (:name)");
+    $stmt->bindParam(':name', $name);
+
+    // Execute the statement
+    $stmt->execute();
+
+    $rootFolder = dirname($_SERVER['PHP_SELF']);
+    header("Location: $rootFolder/fin/test");
 });
 
 
