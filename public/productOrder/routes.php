18fcdaf780e8312a8eea3f8ba5e086df86bc1174
lamed <94333231+lamed47@users.noreply.github.com>
2024-03-22 10:27:46 +0800

    add page

--- public/productOrder/routes.php
+++ public/productOrder/routes.php
@@ -5,6 +5,15 @@ $basePath = "$path/po.";
 
 $po = [
     // Sample Routes
+    '/po/dashboard' => $basePath . "dashboard.php",
+    '/po/suppliers' => $basePath . "suppliers.php",
+    '/po/items' => $basePath . "items.php",
+    '/po/addItem' => $basePath . "addItem.php",
+    '/po/orderRequest' => $basePath . "orderRequest.php",
+    '/po/transactionHistory' => $basePath . "transactionHistory.php",
+
+
+
     '/po/sample' => $basePath . "sample.php",
     '/po/link' => $basePath . "test-link.php",   
 ];
